d9f48d5e58b40d0d31a9458ba49d1dfbb5365f49
Daniel Jacob Archer <me@danieljacobarcher.com>
2014-05-09 17:59:34 -0700

    changed authcode type to text

--- src/handshake/draw.js
+++ src/handshake/draw.js
@@ -71,6 +71,7 @@
     this.authcode_field.className           = "handshake-authcode-field";
     this.authcode_field.id                  = "handshake-authcode-field-id-"+this.uuid;
     this.authcode_field.maxLength           = 4;
+    this.authcode_field.type                = "text";
     this.authcode_field.inputmode           = "numeric";
     this.authcode_field.pattern             = "[0-9]*";
 
